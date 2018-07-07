ce996b5a8325d3fce72c542107102e0107fc9cb7
huyphung1602 <huy.phung.sw@gmail.com>
2018-07-07 19:03:52 +0700

    Add solution 2 for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,9 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-// My solution
+// Solution 1
+// function palindrome(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '') === str;
+// }
+
+// Solution 2
 function palindrome(str) {
-  return str.split('').reduce((rev, char) => char + rev, '') === str;
+  return str.split('').every((char, i) => {
+    return char === str[str.length -i - 1];
+  });
 }
 
 module.exports = palindrome;
