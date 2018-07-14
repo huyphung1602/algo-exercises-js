b76e34a3fdc36c3c9e56c3cbf07e0ac09c221795
huyphung1602 <huy.phung.sw@gmail.com>
2018-07-14 16:37:31 +0700

    Add solution 3 for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -26,23 +26,45 @@
 //   }
 // }
 
-// Solution 2
-function pyramid(n) {
+// // Solution 2
+// function pyramid(n) {
+//   const mid = Math.floor((2*n-1)/2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2*n-1; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+// Solution 3
+function pyramid(n, row = 0, level = '') {
   const mid = Math.floor((2*n-1)/2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
-
-    for (let column = 0; column < 2*n-1; column++) {
-      if (mid - row <= column && mid + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-
-    console.log(level);
+  if (row === n) {
+    return;
   }
+
+  if (2*n-1 === level.length) {
+    console.log(level);
+    return pyramid(n, row+1);
+  }
+
+  if (level.length >= mid - row && level.length <= mid + row) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  return pyramid(n, row, level);
 }
 
 module.exports = pyramid;
